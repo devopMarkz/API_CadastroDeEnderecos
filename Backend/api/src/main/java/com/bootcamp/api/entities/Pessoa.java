bccbc3b74cbfcafd85ed51a185d653b3bdc07bda
devopMarkz <marcosdev2002@gmail.com>
2024-11-30 00:50:10 -0300

    Criando entidade Pessoa

---
+++ Backend/api/src/main/java/com/bootcamp/api/entities/Pessoa.java
@@ -0,0 +1,115 @@
+package com.bootcamp.api.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "TB_PESSOA")
+public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CODIGO_PESSOA")
+    private Long codigoPessoa;
+
+    @Column(name = "NOME")
+    private String nome;
+
+    @Column(name = "SOBRENOME")
+    private String sobrenome;
+
+    @Column(name = "IDADE")
+    private Integer idade;
+
+    @Column(name = "LOGIN", nullable = false, unique = true)
+    private String login;
+
+    @Column(name = "SENHA", nullable = false, unique = true)
+    private String senha;
+
+    @Column(name = "STATUS", nullable = false)
+    private Integer status;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(Long codigoPessoa, String nome, String sobrenome, Integer idade, String login, String senha, Integer status) {
+        this.codigoPessoa = codigoPessoa;
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.idade = idade;
+        this.login = login;
+        this.senha = senha;
+        this.status = status;
+    }
+
+    public Long getCodigoPessoa() {
+        return codigoPessoa;
+    }
+
+    public void setCodigoPessoa(Long codigoPessoa) {
+        this.codigoPessoa = codigoPessoa;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Pessoa pessoa = (Pessoa) object;
+        return Objects.equals(codigoPessoa, pessoa.codigoPessoa);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(codigoPessoa);
+    }
+}
