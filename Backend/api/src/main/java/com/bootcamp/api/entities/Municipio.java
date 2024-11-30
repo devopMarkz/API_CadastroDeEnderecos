a4e2dc1376edd17dd9990517fe2afc0a7b483eb7
devopMarkz <marcosdev2002@gmail.com>
2024-11-30 00:28:17 -0300

    first commit

---
+++ Backend/api/src/main/java/com/bootcamp/api/entities/Municipio.java
@@ -0,0 +1,9 @@
+package com.bootcamp.api.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TB_MUNICIPIO")
+public class Municipio {
+}
---
+++ Backend/api/src/main/java/com/bootcamp/api/entities/Uf.java
@@ -0,0 +1,79 @@
+package com.bootcamp.api.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "TB_UF")
+public class Uf {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CODIGO_UF")
+    private Long codigoUF;
+
+    @Column(name = "SIGLA", unique = true, nullable = false)
+    private String sigla;
+
+    @Column(name = "NOME", unique = true, nullable = false)
+    private String nome;
+
+    @Column(name = "STATUS")
+    private Integer status;
+
+    public Uf() {
+    }
+
+    public Uf(Long codigoUF, String sigla, String nome, Integer status) {
+        this.codigoUF = codigoUF;
+        this.sigla = sigla;
+        this.nome = nome;
+        this.status = status;
+    }
+
+    public Long getCodigoUF() {
+        return codigoUF;
+    }
+
+    public void setCodigoUF(Long codigoUF) {
+        this.codigoUF = codigoUF;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Uf uf = (Uf) object;
+        return Objects.equals(codigoUF, uf.codigoUF);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(codigoUF);
+    }
+}
